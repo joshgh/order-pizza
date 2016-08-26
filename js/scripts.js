ce65e153d6c6163112c61a5674c6cee8cc3b0e5c
Joshua Huffman <j.m.huffman@gmail.com>
2016-08-26 14:09:58 -0700

    move display refresh operations into separate function

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,11 @@ function Order(){
   this.pizzaList = [];
 }
 
+Order.prototype.addPizza = function(pizza){
+  this.pizzaList.push(pizza);
+  return this.pizzaList;
+}
+
 Order.prototype.totalCost = function(){
   var cost = 0;
   this.pizzaList.forEach(function(pizza){
@@ -42,32 +47,34 @@ Pizza.prototype.cost = function(){
 $(document).ready(function() {
   function refreshDisplay(order){
     $(".output").text("");
-    $("#totalCost").text(order.totalCost().toFixed(2))
+    order.pizzaList.forEach(function(pizza, index){
+      $("ul#pizzaList").append("<li><span class='pizza'>Pizza #" + (index + 1) + "</span>" +
+                                " | <span class='remove'> Remove</span></li>");
+      $("#pizzaList li .pizza").last().click(function(){
+        $("li").removeClass("chosen");
+        $(this).parent().addClass("chosen");
+        $(".size").text(pizza.pizzaSize);
+        $(".toppings").text(pizza.toppings);
+        $(".cost").text(pizza.cost().toFixed(2));
+      });
+      $("#pizzaList li .remove").last().click(function(){
+        order.pizzaList.forEach(function(element, index){
+          if(element === pizza){
+            order.pizzaList.splice(index, 1);
+          }
+        });
+        refreshDisplay(order);
+      });
+    });
+    $("#totalCost").text("Total Cost: $" + order.totalCost().toFixed(2));
   }
-
   var myOrder = new Order;
   $("#submit").click(function(){
     var size =  $("input[name='size']:checked"). val();
     var toppings = $("#toppings").val();
     var myPizza = new Pizza(size);
     myPizza.toppings = toppings;
-    myOrder.pizzaList.push(myPizza);
-    $("ul#pizzaList").append("<li><span class='pizza'>Pizza #" + myOrder.pizzaList.length + "</span>" +
-                              "<span class='remove'> Remove</span></li>")
-    $("#pizzaList li .pizza").last().click(function(){
-      $(".size").text(myPizza.pizzaSize);
-      $(".toppings").text(myPizza.toppings);
-      $(".cost").text(myPizza.cost().toFixed(2));
-    })
-    $("#pizzaList li .remove").last().click(function(){
-      $(this).parent().remove();
-      myOrder.pizzaList.forEach(function(element, index){
-        if(element === myPizza){
-          myOrder.pizzaList.splice(index, 1);
-        }
-      })
-      refreshDisplay(myOrder);
-    })
-    $("#totalCost").text(myOrder.totalCost().toFixed(2));
-  })
+    myOrder.addPizza(myPizza);
+    refreshDisplay(myOrder);
+  });
 });
