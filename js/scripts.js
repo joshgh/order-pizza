5d5f2bbfee29c1bce5e833817f4632da44865b0e
Joshua Huffman <j.m.huffman@gmail.com>
2016-08-26 08:38:49 -0700

    create Pizza constructor and prototype

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,24 @@
 
+function Pizza(size){
+  this.pizzaSize = size;
+  this.toppings = [];
+}
+
+Pizza.prototype.cost = function(){
+  var currentCost = 10;
+  if (this.pizzaSize === "m"){
+    currentCost += 2;
+  } else if (this.pizzaSize === "l"){
+    currentCost += 5;
+  } else if (this.pizzaSize === "xl"){
+    currentCost += 8;
+  }
+
+  if (this.toppings.length > 3){
+    currentCost += (this.toppings.length - 3) * 1.5;
+  }
+}
+
 $(document).ready(function() {
 
 });
